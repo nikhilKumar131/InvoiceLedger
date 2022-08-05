c1b6f467448f4f200562021ef6332d813885df67
nikhilKumar131 <109893645+nikhilKumar131@users.noreply.github.com>
2022-08-05 20:09:30 +0000

    Update header.js

--- public/js/header.js
+++ public/js/header.js
@@ -3,18 +3,8 @@ function home(){
 }
 
 function admin(){
-    location.href="http://localhost:3000/admin"
+    location.href="https://invoiceledger.herokuapp.com/admin"
 }
 
 
-/* To connect using MetaMask */
-async function metamask() { if (window.ethereum) {
- 
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    window.web3 = new Web3(window.ethereum);
-    
-   } else {
-    console.log("No wallet");
-   }
-}
 
